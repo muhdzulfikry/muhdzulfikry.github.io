ac9deca93bcadd3554f7f4eac2bf167a66a5aa96
Muhammad Dzul Fikry <106637783+muhdzulfikry@users.noreply.github.com>
2022-08-22 00:50:19 +0700

    Delete Indicator.js

--- Indicator.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './indicator.css';
-
-const Indicator = ({ priority, ...props }) => (
-  <div className={`priority-indicator ${priority}`} {...props} />
-);
-
-export default Indicator;
